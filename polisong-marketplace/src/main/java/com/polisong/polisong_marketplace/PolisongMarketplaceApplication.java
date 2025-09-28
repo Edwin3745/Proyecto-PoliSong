5d8187d3317a7522a1d42afaf81faefb03fdbd87
Edwin3745 <estreyes@poligran.edu.co>
2025-09-27 19:41:32 -0500

    Initial commit - Proyecto base Spring Boot

---
+++ polisong-marketplace/src/main/java/com/polisong/polisong_marketplace/PolisongMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.polisong.polisong_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisongMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolisongMarketplaceApplication.class, args);
+	}
+
+}
---
+++ polisong-marketplace/src/test/java/com/polisong/polisong_marketplace/PolisongMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polisong.polisong_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolisongMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
