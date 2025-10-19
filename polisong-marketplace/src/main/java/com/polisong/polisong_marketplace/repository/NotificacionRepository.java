101e40a202618cb7bc5834a56d0f8f7111be512c
JohanCode07 <johan0code@gmail.com>
2025-10-19 17:45:35 -0500

    Se implementó el repositorio de notificaciones (NotificacionRepository) para gestionar la conexión con la base de datos y facilitar las operaciones de guardado y consulta de notificaciones mediante Spring Data JPA.

--- polisong-marketplace/src/main/java/com/polisong/polisong_marketplace/repository/NotificacionRepository.java
+++ polisong-marketplace/src/main/java/com/polisong/polisong_marketplace/repository/NotificacionRepository.java
@@ -1,9 +1,13 @@
 package com.polisong.polisong_marketplace.repository;
 
 import com.polisong.polisong_marketplace.model.Notificacion;
+import com.polisong.polisong_marketplace.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface NotificacionRepository extends JpaRepository<Notificacion, Integer> {
+    List<Notificacion> findByUsuario(Usuario usuario);
+    List<Notificacion> findByEstado(String estado);
 }
